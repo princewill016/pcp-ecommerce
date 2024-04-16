3d9fe58a8467149324d6750ef0c6092a11fb2cbc
Peter David <Princewill016@gmail.com>
2024-04-16 08:05:23 +0100

    add products

---
+++ src/components /Home.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Products from "./Products";
+
+function Home() {
+  return (
+    <div className="hero">
+      {/* <div
+        id="carouselExampleSlidesOnly"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src="/assets/firstca.jpg"
+              className="d-block w-100"
+              alt="first carosel"
+            />{" "}
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/assets/secondca.jpg"
+              className="d-block w-100"
+              alt="second carosel"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="/assets/thirdca.jpg"
+              className="d-block w-100"
+              alt="third carosel"
+            />
+          </div>
+        </div>
+      </div> */}
+
+      {/* gap */}
+      <div className="card bg-dark text-white border-0">
+        <img
+          src="/assets/cardh.jpg"
+          className="card-img"
+          alt="cardimg"
+          height="550px"
+        />
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="container">
+            <h5 className="card-title display-3 fw-bolder mb-0">
+              Latest Arrivals
+            </h5>
+            <p className="card-text lead fs-2">CHECK OUT ALL NEW TRENDS</p>
+          </div>
+        </div>
+      </div>
+      <Products />
+    </div>
+  );
+}
+
+export default Home;
--- src/components /Navbar.jsx
+++ src/components /Navbar.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function Navbar() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container">
-          <a class="navbar-brand fw-bold fs-4" href="">
+      <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
+        <div className="container">
+          <a className="navbar-brand fw-bold fs-4" href="www.google.com">
             PCP-Shoppings
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -17,39 +17,43 @@ function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="www.google.com"
+                >
                   Home
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li className="nav-item">
+                <a className="nav-link" href="www.google.com">
                   Product
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li className="nav-item">
+                <a className="nav-link" href="www.google.com">
                   About
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li className="nav-item">
+                <a className="nav-link" href="www.google.com">
                   Contact
                 </a>
               </li>
             </ul>
             <div className="buttons">
-              <a href="" className="btn btn-outline-dark">
+              <a href="www.google.com" className="btn btn-outline-dark">
                 <i className="fa fa-sign-in me-1"></i>Login
               </a>
-              <a href="" className="btn btn-outline-dark ms-2">
+              <a href="www.google.com" className="btn btn-outline-dark ms-2">
                 <i className="fa fa-user me-1"></i>Register
               </a>
-              <a href="" className="btn btn-outline-dark ms-2">
+              <a href="www.google.com" className="btn btn-outline-dark ms-2">
                 <i className="fa fa-user me-1"></i>Cart (0)
               </a>
             </div>
